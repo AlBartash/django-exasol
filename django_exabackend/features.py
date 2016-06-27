8778191de8e9528dbe1517313f565b1bf0409e89
Javier Abadía <javi.abadia@gmail.com>
2016-06-27 12:11:01 +0200

    implement support transactions to prevent invocation of default method it causes tests to fail during setUp because parent method tries to create a table, something that may not be possible in general (permissions, no open schema)

--- django_exabackend/features.py
+++ django_exabackend/features.py
@@ -9,3 +9,6 @@ except ImportError: pytz = None
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     datetime_tz_cut = re.compile(r'^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[.]\d{6})[-+]\d{2}:\d{2}$')
+
+    def supports_transactions(self):
+        return True
